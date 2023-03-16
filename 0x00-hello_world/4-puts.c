93bb1e18717ea1ddcfa55822f582c1c729e3f66b
papilo114 <kofiatiefah@gmail.com>
2023-03-16 15:13:02 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ *  main - A program to print a line with puts function
+ *  Return: 0
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
