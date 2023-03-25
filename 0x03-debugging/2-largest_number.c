b29f7173bc8a34340facc4ab5cd25123a7faf4b2
papilo114 <kofiatiefah@gmail.com>
2023-03-24 23:15:27 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,21 +9,21 @@
  */
 
 int largest_number(int a, int b, int c)
-
 {
 	int largest;
-
-	if (a >= b && b >= c)
+	
+	if (a > b && b > c)
 	{
-	largest = a;
+		largest = a;
 	}
-	else if (b >= a && a >= c)
+    else if (b > a && a > c)
 	{
-	largest = b;
-	}
-	else
+		largest = b;
+	}	
+    else
 	{
-	largest = c;
+		largest = c;
 	}
-	return (largest);
+    
+    return (largest);
 }
